b2d843e95d24fd6a046b6dbea6db36caa11d9e57
firas-frikha <frikha.firas@outlook.com>
2025-04-14 23:47:01 +0200

    WIP

--- MillenniumFalconBackend/src/test/scala/application/DefaultRoutesQueryServiceSpec.scala
+++ MillenniumFalconBackend/src/test/scala/application/DefaultRoutesQueryServiceSpec.scala
@@ -1,5 +1,103 @@
 package application
 
-class DefaultRoutesQueryServiceSpec {
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.stream.alpakka.slick.scaladsl.SlickSession
+import com.typesafe.config.ConfigFactory
+import infrastructure.DefaultRoutesQueryService
+import model.Route
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.Outcome
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.wordspec.FixtureAnyWordSpec
+import org.slf4j.LoggerFactory
+import repository.RoutesSchema
 
+import java.util.UUID
+import scala.util.Random
+
+class DefaultRoutesQueryServiceSpec
+  extends FixtureAnyWordSpec
+    with ScalaFutures
+    with Matchers
+    with MockFactory {
+
+  override type FixtureParam = TestFixture
+
+  final class TestFixture(val actorTestKit: ActorTestKit,
+                          val slickSession: SlickSession,
+                          val routesSchema: RoutesSchema)
+
+  final val Logger = LoggerFactory.getLogger(getClass)
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    val config =
+      ConfigFactory.parseString(s"testDatabaseName = test-${UUID.randomUUID().toString}")
+        .withFallback(ConfigFactory.parseResourcesAnySyntax("application-test-query.conf"))
+        .resolve()
+
+    val actorTestKit = ActorTestKit(
+      s"{ActorTestKitBase.testNameFromCallStack()}-${UUID.randomUUID().toString}",
+      config
+    )
+
+    val slickSession = SlickSession.forConfig("slick", config)
+    val routesSchema = new RoutesSchema()
+    import slickSession.profile.api._
+
+    try {
+      val operations = DBIO.seq(
+        routesSchema.routesTable.schema.create,
+      )
+      whenReady(slickSession.db.run(operations), timeout(Span(6, Seconds)), interval(Span(2, Seconds))) { _ =>
+        Logger.info("routes schema table has been created.")
+      }
+      withFixture(test.toNoArgTest(new TestFixture(actorTestKit, slickSession, routesSchema)))
+    } finally {
+      val operations = DBIO.seq(
+        routesSchema.routesTable.schema.dropIfExists,
+
+      )
+      whenReady(slickSession.db.run(operations), timeout(Span(6, Seconds))) { _ =>
+        Logger.info("routes schema table has been dropped.")
+      }
+      actorTestKit.shutdownTestKit()
+    }
+  }
+
+  "DefaultQueryService" when {
+    "fetching all routes" must {
+      val routes = Seq(
+        Route(
+          origin = Random.alphanumeric.take(12).mkString,
+          destination = Random.alphanumeric.take(12).mkString,
+          travelTime = Random.nextInt()
+        ),
+        Route(
+          origin = Random.alphanumeric.take(12).mkString,
+          destination = Random.alphanumeric.take(12).mkString,
+          travelTime = Random.nextInt()
+        )
+      )
+
+      "return valid result" in { testFixture =>
+        import testFixture.slickSession.profile.api._
+        import testFixture.{routesSchema, slickSession}
+
+        val operations = DBIO.seq(
+          routesSchema.routesTable ++= routes
+        )
+        whenReady(slickSession.db.run(operations.transactionally), timeout(Span(5, Seconds))) { _ =>
+
+          val queryService = new DefaultRoutesQueryService(routesSchema, slickSession)
+
+          queryService.fetchAll()
+            .futureValue(timeout(Span(5, Seconds)))
+            .sortBy(_.origin)
+            .mustBe(routes.sortBy(_.origin))
+        }
+      }
+    }
+  }
 }
--- MillenniumFalconBackend/src/test/scala/application/DefaultSurvivalComputationServiceSpec.scala
+++ MillenniumFalconBackend/src/test/scala/application/DefaultSurvivalComputationServiceSpec.scala
@@ -1,5 +1,60 @@
 package application
 
-class DefaultSurvivalComputationServiceSpec {
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.scaladsl.Behaviors
+import model.BountyHuntersData
+import model.BountyHuntersData.AttackPlan
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
 
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
+class DefaultSurvivalComputationServiceSpec
+  extends ScalaTestWithActorTestKit
+    with AnyWordSpecLike
+    with Matchers
+    with ScalaFutures {
+
+  "DefaultSurvivalComputationService" should {
+
+    "return the survival percentage when the actor replies with a SurvivalPercentage" in {
+      val survivalComputationActor = testKit.spawn(Behaviors.receiveMessage[SurvivalComputationActor.Input] {
+        case SurvivalComputationActor.Compute(_, replyTo) =>
+          replyTo ! SurvivalComputationActor.SurvivalPercentage(0.75)
+          Behaviors.same
+      })
+
+      val service = new DefaultSurvivalComputationService(survivalComputationActor)(system)
+
+      val bountyData = BountyHuntersData(Random.nextInt(),
+        Seq(AttackPlan(Random.alphanumeric.take(12).mkString, Random.nextInt())))
+
+      val futureResult = service.computeSurvivalProbability(bountyData)
+
+      futureResult.futureValue shouldEqual 0.75
+    }
+
+    "fail with IllegalStateException when the actor replies with FailedSurvivalComputation" in {
+
+      val exception = new Exception("Unknown exception")
+      val survivalComputationActor = testKit.spawn(Behaviors.receiveMessage[SurvivalComputationActor.Input] {
+        case SurvivalComputationActor.Compute(_, replyTo) =>
+          replyTo ! SurvivalComputationActor.FailedSurvivalComputation(exception.getMessage)
+          Behaviors.same
+      })
+
+      val service = new DefaultSurvivalComputationService(survivalComputationActor)(system)
+
+      val bountyData = BountyHuntersData(Random.nextInt(),
+        Seq(AttackPlan(Random.alphanumeric.take(12).mkString, Random.nextInt())))
+
+      val futureResult = service.computeSurvivalProbability(bountyData)
+
+      val thrown = futureResult.failed.futureValue
+      thrown shouldBe a [IllegalStateException]
+      thrown.getMessage shouldEqual exception.getMessage
+    }
+  }
 }
