3139eac1e4956f1528c029f8b7a17fcd633d5a14
firas-frikha <frikha.firas@outlook.com>
2025-04-15 11:45:27 +0200

    WIP

--- MillenniumFalconBackend/src/main/scala/infrastructure/SlickSessionProvider.scala
+++ MillenniumFalconBackend/src/main/scala/infrastructure/SlickSessionProvider.scala
@@ -4,6 +4,8 @@ import akka.actor.typed.{ActorSystem, Extension, ExtensionId}
 import akka.stream.alpakka.slick.scaladsl.SlickSession
 import com.typesafe.config.{Config, ConfigFactory}
 
+import java.nio.file.Paths
+
 class SlickSessionProvider(databasePath: String) extends Extension {
   val configStr =
     s"""
@@ -27,9 +29,16 @@ class SlickSessionProvider(databasePath: String) extends Extension {
 object SlickSessionProvider extends ExtensionId[SlickSessionProvider] {
 
   override def createExtension(system: ActorSystem[_]): SlickSessionProvider = {
-    val dataBasePAth = system.extension(MillenniumFalconConfiguration).databaseUrl
 
-    new SlickSessionProvider(dataBasePAth)
+    val dataBasePath = system.extension(MillenniumFalconConfiguration).databaseUrl
+    val resolvedPath = {
+      val path = Paths.get(dataBasePath)
+      if (path.isAbsolute) path.toString
+      else {
+        val baseDir = Paths.get(System.getProperty("user.dir"))
+        baseDir.resolve(path).toAbsolutePath.toString
+      }
+    }
+    new SlickSessionProvider(resolvedPath)
   }
-
 }
