cf7290bad5290f59b4043e35b84a07fe961e1719
firas-frikha <frikha.firas@outlook.com>
2025-04-23 18:29:51 +0200

    WIP

--- MillenniumFalconFrontEnd/src/environments/environment.prod.ts
+++ MillenniumFalconFrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://millennium-falcon-backend:8080'
+  apiUrl: ''
 };
--- MillenniumFalconFrontEnd/src/environments/environment.ts
+++ MillenniumFalconFrontEnd/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://millennium-falcon-backend:8080'
+  apiUrl: '',
 };
